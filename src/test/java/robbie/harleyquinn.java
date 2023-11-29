39857f832797c43ca856e3e48964ede27f38b104
KarthikN94 <karthitheoptimist@gmail.com>
2023-11-29 19:58:47 +0530

    information

---
+++ src/test/java/robbie/harleyquinn.java
@@ -0,0 +1,23 @@
+package robbie;
+
+public class harleyquinn {
+
+	private void Ironman() {
+		System.out.println("Edge Armour");
+	}
+
+	private void Wanda() {
+		System.out.println("Telekinesis");
+	}
+	private void thor() {
+		System.out.println("Lightening and Thunder");
+	}
+
+	private void captainamerica() {
+		System.out.println("Supersoldier serum");
+	}
+
+
+
+		}
+
